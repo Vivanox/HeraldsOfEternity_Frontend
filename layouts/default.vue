526eb4949b4da1f94daeb9b30d76d894a83132a6
Jonas Kervin Hansen <jonas.kerwin.hansen@gmail.com>
2020-08-15 21:37:41 +0200

    Footer with social links

--- layouts/default.vue
+++ layouts/default.vue
@@ -115,6 +115,60 @@
         <Nuxt />
       </main>
     </div>
+
+    <footer class="bg-white-smoke">
+      <div
+        class="max-w-screen-xl mx-auto py-4 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8"
+      >
+        <div class="flex justify-center md:order-1">
+          <a
+            href="https://www.facebook.com/vivanox"
+            class="text-gray-400 hover:text-gray-500"
+          >
+            <span class="sr-only">Facebook</span>
+            <svg class="h-16 w-16" fill="currentColor" viewBox="0 0 24 24">
+              <path
+                fill-rule="evenodd"
+                d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </a>
+
+          <a
+            href="https://twitter.com/vivanox_ivs"
+            class="ml-6 text-gray-400 hover:text-gray-500"
+          >
+            <span class="sr-only">Twitter</span>
+            <svg class="h-16 w-16" fill="currentColor" viewBox="0 0 24 24">
+              <path
+                d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"
+              />
+            </svg>
+          </a>
+
+          <a
+            href="https://discord.gg/e4348S"
+            class="ml-6 text-gray-400 hover:text-gray-500"
+          >
+            <span class="sr-only">Discord</span>
+            <svg class="h-16 w-16" fill="currentColor" viewBox="0 0 245 240">
+              <path
+                d="M104.4 103.9c-5.7 0-10.2 5-10.2 11.1s4.6 11.1 10.2 11.1c5.7 0 10.2-5 10.2-11.1.1-6.1-4.5-11.1-10.2-11.1zM140.9 103.9c-5.7 0-10.2 5-10.2 11.1s4.6 11.1 10.2 11.1c5.7 0 10.2-5 10.2-11.1s-4.5-11.1-10.2-11.1z"
+              />
+              <path
+                d="M189.5 20h-134C44.2 20 35 29.2 35 40.6v135.2c0 11.4 9.2 20.6 20.5 20.6h113.4l-5.3-18.5 12.8 11.9 12.1 11.2 21.5 19V40.6c0-11.4-9.2-20.6-20.5-20.6zm-38.6 130.6s-3.6-4.3-6.6-8.1c13.1-3.7 18.1-11.9 18.1-11.9-4.1 2.7-8 4.6-11.5 5.9-5 2.1-9.8 3.5-14.5 4.3-9.6 1.8-18.4 1.3-25.9-.1-5.7-1.1-10.6-2.7-14.7-4.3-2.3-.9-4.8-2-7.3-3.4-.3-.2-.6-.3-.9-.5-.2-.1-.3-.2-.4-.3-1.8-1-2.8-1.7-2.8-1.7s4.8 8 17.5 11.8c-3 3.8-6.7 8.3-6.7 8.3-22.1-.7-30.5-15.2-30.5-15.2 0-32.2 14.4-58.3 14.4-58.3 14.4-10.8 28.1-10.5 28.1-10.5l1 1.2c-18 5.2-26.3 13.1-26.3 13.1s2.2-1.2 5.9-2.9c10.7-4.7 19.2-6 22.7-6.3.6-.1 1.1-.2 1.7-.2 6.1-.8 13-1 20.2-.2 9.5 1.1 19.7 3.9 30.1 9.6 0 0-7.9-7.5-24.9-12.7l1.4-1.6s13.7-.3 28.1 10.5c0 0 14.4 26.1 14.4 58.3 0 0-8.5 14.5-30.6 15.2z"
+              />
+            </svg>
+          </a>
+        </div>
+        <div class="mt-8 md:mt-0 md:order-2">
+          <p class="text-center text-xl leading-6 text-gray-400">
+            &copy; {{ year }} Vivanox. {{ $t('All rights reserved') }}.
+          </p>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -181,6 +235,10 @@ export default {
     ],
   }),
   computed: {
+    year() {
+      return new Date().getFullYear()
+    },
+
     backgroundURL() {
       return require('~/static/Background01_Space.png')
     },
