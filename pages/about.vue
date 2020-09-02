e6ba12fc7beb9f8b6ea10a19266d4d1e4ad5cfc0
Jonas <jonas.kerwin.hansen@gmail.com>
2020-09-02 10:16:16 +0200

    Move about into separate from index / home page

---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Gallery />
+
+    <Herald-overview />
+
+    <Role-overview />
+  </div>
+</template>
+
+<script>
+import Gallery from '~/components/About/Gallery'
+import HeraldOverview from '~/components/About/HeraldOverview'
+import RoleOverview from '~/components/About/RoleOverview'
+
+export default {
+  components: {
+    Gallery,
+    HeraldOverview,
+    RoleOverview,
+  },
+}
+</script>
