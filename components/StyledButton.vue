bfaf18e61e8e4269ab6b132736b783a648b5844e
Jonas <jonas.kerwin.hansen@gmail.com>
2020-09-06 16:02:48 +0200

    Mobile scale

--- components/StyledButton.vue
+++ components/StyledButton.vue
@@ -1,9 +1,10 @@
 <template>
-  <span class="inline-flex group p-2" :class="spanCSSClasses" v-on="$listeners">
+  <span class="inline-flex group p-2" :class="spanCSSClasses">
     <button
       type="button"
-      class="inline-flex items-center px-48 py-10 border-4 transition ease-in-out duration-150"
+      class="inline-flex items-center px-10 py-4 xl:px-48 xl:py-10 border-4 transition ease-in-out duration-150"
       :class="buttonCSSClasses"
+      v-on="$listeners"
     >
       <slot />
     </button>
@@ -27,7 +28,7 @@ export default {
     },
 
     buttonCSSClasses() {
-      return ' border-dark-blue text-6xl leading-6 font-bold text-white bg-orange-500 group-hover:bg-orange-gold focus:outline-none focus:border-orange-700 focus:shadow-outline-orange active:bg-orange-700'
+      return ' border-dark-blue text-sm md:text-lg xl:text-6xl leading-5 lg:leading-6 font-bold text-white bg-orange-500 group-hover:bg-orange-gold focus:outline-none focus:border-orange-700 focus:shadow-outline-orange active:bg-orange-700'
     },
   },
 }
--- pages/heralds.vue
+++ pages/heralds.vue
@@ -6,7 +6,7 @@
       {{ $t('Choose your Herald') }}
     </h1>
 
-    <div class="grid grid-cols-4 gap-4">
+    <div class="grid grid-cols-4 gap-4 px-4">
       <nuxt-link
         v-for="herald in heralds"
         :key="herald.name"
