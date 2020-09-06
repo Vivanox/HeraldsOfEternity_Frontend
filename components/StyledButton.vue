d4365dab5ff32fea6a2f4ef9645ee7bc40396053
Jonas <jonas.kerwin.hansen@gmail.com>
2020-09-06 16:14:40 +0200

    responsiveness

--- components/StyledButton.vue
+++ components/StyledButton.vue
@@ -2,7 +2,7 @@
   <span class="inline-flex group p-2" :class="spanCSSClasses">
     <button
       type="button"
-      class="inline-flex items-center px-10 py-4 xl:px-48 xl:py-10 border-4 transition ease-in-out duration-150"
+      class="inline-flex items-center px-10 py-4 md:px-24 md:py-10 xl:px-48 xl:py-10 border-4 transition ease-in-out duration-150"
       :class="buttonCSSClasses"
       v-on="$listeners"
     >
@@ -28,7 +28,7 @@ export default {
     },
 
     buttonCSSClasses() {
-      return ' border-dark-blue text-sm md:text-lg xl:text-6xl leading-5 lg:leading-6 font-bold text-white bg-orange-500 group-hover:bg-orange-gold focus:outline-none focus:border-orange-700 focus:shadow-outline-orange active:bg-orange-700'
+      return ' border-dark-blue text-4xl md:text-6xl leading-5 lg:leading-6 font-bold text-white bg-orange-500 group-hover:bg-orange-gold focus:outline-none focus:border-orange-700 focus:shadow-outline-orange active:bg-orange-700'
     },
   },
 }
