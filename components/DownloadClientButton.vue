4e737a816948849147d590c75b9f57c662b849c8
Jonas <jonas.kerwin.hansen@gmail.com>
2020-12-03 19:06:32 +0100

    Updated download links

--- components/DownloadClientButton.vue
+++ components/DownloadClientButton.vue
@@ -10,11 +10,11 @@ export default {
     downloadClient() {
       if (navigator.platform.includes('Win')) {
         window.open(
-          'https://drive.google.com/file/d/1hBZKjnpLEBgHQVjLbOQql5TiKG2rtoxU/view?usp=sharing'
+          'https://drive.google.com/file/d/1UfDtLWOum0RkAZ8-nCAC2_w7xdoppH8U/view?usp=sharing'
         )
       } else if (navigator.platform.includes('Lin')) {
         window.open(
-          'https://drive.google.com/file/d/1Ov3zpplX7DqEg5c582ZxGc_ksfs36lIi/view?usp=sharing'
+          'https://drive.google.com/file/d/1RUkuzQyKQ43PcXfdsdem2ulkhlE6MAIo/view?usp=sharing'
         )
       } else {
         window.alert('No client for your OS, yet.')
